d584b4c75f0fb5dd3757da6622cfd938d99e2623
ivelisseg5 <ivelissegarcia121503@gmail.com>
2024-12-11 20:30:14 -0800

    wrote my solution for count-occurrences.js

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,17 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    // initialize the count variable
+    let count = 0;
+    
+    // i is the index, so we're looping through the string to get the index of each character in the string
+    // and increment the count variable to count how many times that specific character appeared.
+    for (let i = 0; i < str.length; i++){    
+        if(str[i] === char){
+            count++;
+        }
+    }
+
+    // return the count variable
+    return count;
+}
 
 module.exports = countOccurrences;
