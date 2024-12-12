3741de859e784a53118d6b4b2e9c69399c7a525e
ivelisseg5 <ivelissegarcia121503@gmail.com>
2024-12-11 20:12:27 -0800

    fixed equal signs, added comments

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -2,21 +2,20 @@ function calculator(num1, num2, operator) {
     
     let number;
 
-    if (operator = '+'){
+    if (operator == '+'){ // the double equal signs mean "if this variable is equal to this value"
         return number = num1 + num2;
-    }
-    
-    if (operator = '-'){
+    } else if (operator == '-'){
         return number = num1 - num2;
-    } 
-    
-    if (operator = '*'){
+    } else if (operator == '*'){
         return number = num1 * num2;
-    } 
-    
-    if (operator = '/'){
+    } else if (operator == '/'){
         return number = num1 / num2;
+    }else {
+        throw new Error('Invalid operator');
     }
+    
+    // triple equal signs are like booleans & compare to see if the two variables are the exact same value
+    // my previous mistake was a syntax error in which i used a signle equal sign for everything
 }
 
 module.exports = calculator;
