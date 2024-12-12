71c8e2188ca5d0d1969ad50d4628344bf33ed206
ivelisseg5 <ivelissegarcia121503@gmail.com>
2024-12-11 20:48:49 -0800

    wrote my find max number solution

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,18 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+// have the max value be the first index of the array
+    let max = arr[0];
+
+// loop through the array and compare the two values, max and the index in which we're
+// currently in, and if the current index is larger than the previous maximum value
+// replace the max value with the current one (i explained this so badly)
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i]
+        }
+    }
+
+// return the max value
+    return max;
+}
 
 module.exports = findMaxNumber;
