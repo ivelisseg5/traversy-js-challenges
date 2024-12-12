5f6c5710e072b189adac20bfede4a2109a049a70
ivelisseg5 <ivelissegarcia121503@gmail.com>
2024-12-11 19:51:59 -0800

    test cases & my solutions

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    
+    let number;
+
+    if (operator = '+'){
+        return number = num1 + num2;
+    }
+    
+    if (operator = '-'){
+        return number = num1 - num2;
+    } 
+    
+    if (operator = '*'){
+        return number = num1 * num2;
+    } 
+    
+    if (operator = '/'){
+        return number = num1 / num2;
+    }
+}
 
 module.exports = calculator;
